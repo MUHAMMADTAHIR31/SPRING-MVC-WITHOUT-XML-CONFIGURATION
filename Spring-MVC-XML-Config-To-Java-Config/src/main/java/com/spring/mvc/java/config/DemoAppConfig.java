2724130b3cc4fbffcbb7dedcbe2eff56003a1269
Muhammad Tahir <64077988+MUHAMMADTAHIR31@users.noreply.github.com>
2022-04-17 06:52:31 +0500

    Add files via upload

---
+++ Spring-MVC-XML-Config-To-Java-Config/src/main/java/com/spring/mvc/java/config/DemoAppConfig.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.mvc.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ *
+ * @author Dell
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.spring.mvc")
+public class DemoAppConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public ViewResolver viewResolver(){
+        
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        
+        // set prefix 
+        viewResolver.setPrefix("/WEB-INF/view/");
+       
+        //set suffix
+        viewResolver.setSuffix(".jsp");
+        
+        // return the view Resovler
+        return viewResolver;
+    }
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
---
+++ Spring-MVC-XML-Config-To-Java-Config/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Apr 17, 2022, 5:55:46 AM
+    Author     : Dell
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello App With Java Configuration!</h1>
+    </body>
+</html>
